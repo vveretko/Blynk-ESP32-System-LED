b97b13d414ec4f6af3935ac5f274d4dece64fda4
Vitaliy <72790181+vveretko@users.noreply.github.com>
2023-06-02 16:05:26 +0300

    Update Blynk_ESP32_LED.ino
    Added comments

--- Blynk_ESP32_LED.ino
+++ Blynk_ESP32_LED.ino
@@ -1,31 +1,39 @@
-#define BLYNK_TEMPLATE_ID "TMPL8T7DjYTdo"
-#define BLYNK_TEMPLATE_NAME "Blinking Built In LED using ESP32"
-#define BLYNK_AUTH_TOKEN "esg54D2Y-Yl6SYE9Q3A6k_p5_KbTDcdh"
-#define BLYNK_SERVER "blynk-qa.com"
+#define BLYNK_TEMPLATE_ID "TMPLxxxxxx"
+#define BLYNK_TEMPLATE_NAME "Device"
+#define BLYNK_AUTH_TOKEN "YourAuthToken"
 
+//Adding the required libraries
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <BlynkSimpleEsp32_SSL.h>
 
 #define LED_PIN 2
 
-char ssid[] = "vveretko";
-char pass[] = "25252525";
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
 
 void setup()
 {
   pinMode(LED_PIN, OUTPUT);
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, BLYNK_SERVER);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
 }
 
 void loop()
 {
   Blynk.run();
 }
-
+// This function will be called every time
+// Blynk writes values to the Virtual Pin V0
 BLYNK_WRITE(V0)
 {
   int value = param.asInt();
+  // assigning incoming value from pin V0 to a variable
+  // process received value
 
   if (value == 1)
   {
